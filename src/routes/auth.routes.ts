0b6711179984ca68742231479ec19a495dda9fd7
KessNelly <ojkessnelly@gmail.com>
2026-09-12 09:12:25 -0400

    removed old forgot-password route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -7,7 +7,6 @@ const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 router.post("/forgot-password", forgotPasswordLimiter, forgotPassword);
 
